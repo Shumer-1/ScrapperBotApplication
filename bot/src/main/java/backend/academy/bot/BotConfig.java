ab1b0dae96cab47a1ea4ffc601bfec10098ab09e
Shumer-1 <borisov1908vlad@mail.ru>
2025-03-02 22:55:18 +0300

    feat(config): add beans to bot config

--- bot/src/main/java/backend/academy/bot/BotConfig.java
+++ bot/src/main/java/backend/academy/bot/BotConfig.java
@@ -1,9 +1,22 @@
 package backend.academy.bot;
 
+import com.pengrad.telegrambot.TelegramBot;
 import jakarta.validation.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
-public record BotConfig(@NotEmpty String telegramToken) {}
+public record BotConfig(@NotEmpty String telegramToken) {
+    @Bean
+    public TelegramBot telegramBot(BotConfig botConfig) {
+        return new TelegramBot(botConfig.telegramToken());
+    }
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder().build();
+    }
+}
