f55a6c6dfb1c11578310d5e99d400bacdeb0a82d
Shumer-1 <borisov1908vlad@mail.ru>
2025-03-13 19:26:29 +0300

    feat(service): add notification service

---
+++ bot/src/main/java/backend/academy/bot/service/NotificationService.java
@@ -0,0 +1,36 @@
+package backend.academy.bot.service;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.request.SendMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+    private final TelegramBot telegramBot;
+    private static final Logger log = LoggerFactory.getLogger(NotificationService.class);
+
+    public NotificationService(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    public void notify(String message, long userId) {
+        SendMessage sendMessage = new SendMessage(userId, message);
+        try {
+            telegramBot.execute(sendMessage);
+            log.info(
+                "Уведомление успешно отправлено: действие={}, сообщение={}, id пользователя={}",
+                "отправлено",
+                message,
+                userId);
+        } catch (Exception e) {
+            log.error(
+                "Ошибка при отправке уведомления: действие={}, сообщение={}, id пользователя={}, ошибка={}",
+                "отправка",
+                message,
+                userId,
+                e.getMessage());
+        }
+    }
+}
