0a19f9b46fe86caf3b19fe452c2ee1cbdbcb1be0
Shumer-1 <borisov1908vlad@mail.ru>
2025-03-30 23:00:58 +0300

    feat: update bot application and config

--- bot/src/main/java/backend/academy/bot/BotApplication.java
+++ bot/src/main/java/backend/academy/bot/BotApplication.java
@@ -2,9 +2,10 @@ package backend.academy.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableConfigurationProperties({BotConfig.class})
 public class BotApplication {
     public static void main(String[] args) {
--- bot/src/main/java/backend/academy/bot/BotConfig.java
+++ bot/src/main/java/backend/academy/bot/BotConfig.java
@@ -2,16 +2,19 @@ package backend.academy.bot;
 
 import com.pengrad.telegrambot.TelegramBot;
 import jakarta.validation.constraints.NotEmpty;
+import java.util.concurrent.Executor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 @EnableAsync
-public record BotConfig(@NotEmpty String telegramToken) {
+public record BotConfig(@NotEmpty String telegramToken) implements AsyncConfigurer {
     @Bean
     public TelegramBot telegramBot(BotConfig botConfig) {
         return new TelegramBot(botConfig.telegramToken());
@@ -20,5 +23,22 @@ public record BotConfig(@NotEmpty String telegramToken) {
     @Bean
     public WebClient webClient() {
         return WebClient.builder().build();
+    }
+
+    @Bean(name = "botTaskExecutor")
+    public Executor botTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(4);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("AsyncNotifier-");
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        return botTaskExecutor();
     }
 }
