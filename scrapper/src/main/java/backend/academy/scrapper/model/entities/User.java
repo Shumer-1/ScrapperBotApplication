6c725d2fd4841cbd20a6b5275b93b58f7eea7b22
Shumer-1 <borisov1908vlad@mail.ru>
2025-04-03 16:19:50 +0300

    fix: make field eager

--- scrapper/src/main/java/backend/academy/scrapper/model/entities/User.java
+++ scrapper/src/main/java/backend/academy/scrapper/model/entities/User.java
@@ -2,6 +2,7 @@ package backend.academy.scrapper.model.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -23,7 +24,7 @@ public class User {
     @Column(name = "username", nullable = false, unique = true)
     private String username;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
     private List<Link> trackingLinks;
 
     public User() {}
