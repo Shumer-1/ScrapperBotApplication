47265742cc66042a7e53bba0da8af51189d380d1
Shumer-1 <borisov1908vlad@mail.ru>
2025-04-11 22:48:05 +0300

    feat(exceptions): add some exceptions

---
+++ scrapper/src/main/java/backend/academy/scrapper/exceptions/DataAccessException.java
@@ -0,0 +1,21 @@
+package backend.academy.scrapper.exceptions;
+
+public class DataAccessException extends RuntimeException {
+
+    public DataAccessException() {
+        super();
+    }
+
+    public DataAccessException(String message) {
+        super(message);
+    }
+
+    public DataAccessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DataAccessException(Throwable cause) {
+        super(cause);
+    }
+}
+
